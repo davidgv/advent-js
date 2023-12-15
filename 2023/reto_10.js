acdd124e6458e5d815ea7c5cbf3f669b31e2f017
David Gutiérrez <buzon@davidgv.com>
2023-12-15 19:34:33 +0100

    Reto 10 done!

---
+++ 2023/reto_10.js
@@ -0,0 +1,29 @@
+function createChristmasTree(ornaments, height) {
+  let arbol = "";
+  let decoIndex = 0; // ornaments[decoIndex] = elemento a dibujar
+
+  // ramas
+  for (let altura=1; altura<=height; altura++) {
+    arbol += " ".repeat(height-altura);
+    for (let i=1; i<=altura; i++) {
+      arbol += ornaments[decoIndex];
+      if (decoIndex == ornaments.length-1) {
+        decoIndex = 0;
+      } else {
+        decoIndex++;
+      }
+      if (i != altura) {
+        arbol += " "; // espacio entre decoraciones
+      } else {
+        arbol += "\n"; // fin de altura
+      }
+    }
+  }
+
+  // tronco
+  arbol += " ".repeat(height-1)+"|\n";
+
+  return arbol;
+}
+
+console.log(createChristmasTree("123", 4));
