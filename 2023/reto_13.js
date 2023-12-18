cfbcd53b0feb65e2bcfb2c9ac2cba2746a4f0a52
David Gutiérrez <buzon@davidgv.com>
2023-12-18 19:48:05 +0100

    Reto 13 done!

--- 2023/reto_13.js
+++ 2023/reto_13.js
@@ -1,21 +1,31 @@
 function calculateTime(deliveries) {
-  let totalHor = 0;
-  let totalMin = 0;
-  let totalSeg = 0;
-  deliveries.forEach(tiempoEntrega => {
-    totalHor += parseInt(tiempoEntrega[0]);
-    totalMin += parseInt(tiempoEntrega[1]);
-    totalSeg += parseInt(tiempoEntrega[2]);
+
+  function hora2Segundos(h, m, s) {
+  // Convierte h:m:s a segundos
+    return (parseInt(h)*3600 + parseInt(m)*60 + parseInt(s));
+  }
+
+  function segundos2Hora(s) {
+  // Convierte segundos a "hh:mm:ss"
+    let hor = Math.floor(s/3600);
+    let horStr = hor.toString().padStart(2,"0");
+    let min = Math.floor((s-(hor*3600))/60);
+    let minStr = min.toString().padStart(2,"0");
+    let seg = s-(hor*3600)-(min*60);
+    let segStr = seg.toString().padStart(2,"0");
+    return horStr+":"+minStr+":"+segStr;
+  }
+
+  let totalSegEntregas = 0;
+  deliveries.forEach(entrega => {
+    arrEntregas = entrega.split(":");
+    totalSegEntregas += hora2Segundos(arrEntregas[0], arrEntregas[1], arrEntregas[2]);
   });
-  totalMin += Math.floor(totalSeg/60);
-  totalHor += Math.floor(totalMin);
 
-  console.log([totalHor, totalMin, totalSeg]);
+  if (totalSegEntregas >= (7*3600)) {
+    return segundos2Hora(totalSegEntregas-(7*3600));
+  } else {
+    return "-" + segundos2Hora((7*3600)-totalSegEntregas);
+  }
+  
 }
-
-calculateTime([
-  '00:45:00',
-  '00:45:00',
-  '00:00:30',
-  '00:00:30'
-]) // '-05:29:00'
